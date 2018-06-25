dfb18086b7e227332f734eada85585f19991a525
Valientin <valientin.kurzhii5@gmail.com>
2018-06-25 15:44:15 +0300

    Some fixed

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -3,14 +3,16 @@ import { connect } from 'react-redux';
 
 import './home.css';
 
+import { title, description } from './strings';
+
 export class Home extends React.Component {
 
 	render(){
 		return(
 			<div className="root-wrapper">
                 <div className="left-block">
-                    <h1>Dairy App</h1>
-                    <h4>Comment with no sense</h4>
+                    <h1>{title}</h1>
+                    <h4>{description}</h4>
                 </div>
                 <div className="right-block">
 
